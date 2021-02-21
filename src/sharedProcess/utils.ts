0802112fc40bacb0788b224741797127512478af
xatier <xatierlike@gmail.com>
2021-02-21 13:57:30 -0800

    feat: tweak exec behavior

--- src/sharedProcess/utils.ts
+++ src/sharedProcess/utils.ts
@@ -1,13 +1,11 @@
 import { spawn as cpSpawn } from 'child_process'
 
 const exec = (command: string) => {
-	const cmd = cpSpawn('bash', [ '-c', command ], { env: process.env, detached: true })
-	cmd.stdout.on('data', (data) => {
-		console.log(`stdout: ${data}`)
-	})
-
-	cmd.stderr.on('data', (data) => {
-		console.error(`stderr: ${data}`)
+	const cmd = cpSpawn(command, {
+		env: process.env,
+		detached: true,
+		shell: 'bash',
+		stdio: [ 'ignore', 'inherit', 'inherit' ],
 	})
 
 	cmd.unref()
