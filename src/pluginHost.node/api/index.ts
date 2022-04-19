53a8e47027d14afe577adf7d79aeae7af9957613
David Kuo <me@davy.tw>
2022-04-20 00:09:14 +0800

    fix(pluginHost-api): import constants from api package

--- src/pluginHost.node/api/index.ts
+++ src/pluginHost.node/api/index.ts
@@ -1,8 +1,13 @@
+import * as API from 'einstein'
+
 import { methods } from './methods'
 
 export const generateAPI = () => ({
+	// import constants
+	...API,
+
 	// eslint-disable-next-line global-require
-	version: require('@/../package.json').version,
+	version: require('@/../package.json').version as string,
 
 	...methods,
 })
