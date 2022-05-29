6c0ab05e0f9a2554e659708438346ba0996cbda0
xatier <xatierlike@gmail.com>
2022-05-29 05:24:24 -0500

    fix(plugin/search): rebase to origin/dev tip
    - use `spawn` from einstein package
    - load icon from static asset [1]
    - remove now unused constants.ts and utils.ts
    
    [1]:
    https://www.flaticon.com/premium-icon/search_954591?related_id=954591

--- plugins/search/index.ts
+++ plugins/search/index.ts
@@ -1,6 +1,4 @@
-import { PluginContext, SearchResult, VOID_TRIGGER } from 'einstein'
-
-import { exec, findIcon } from './utils'
+import { PluginContext, SearchResult, VOID_TRIGGER, spawn } from 'einstein'
 
 // trigger, url, description
 type engine = [string, string, string]
@@ -15,13 +13,13 @@ const ENGINES: engine[] = [
 	[ 'q', 'https://www.qwant.com/?r=US&sr=en&l=en_gb&h=0&s=0&a=1&b=1&vt=1&hc=0&smartNews=1&theme=0&i=1&q=%s', 'Qwant' ],
 ]
 
-const openSearch = (e:engine, term: string) => {
+const openSearch = (e:engine, term: string, icon: string): SearchResult => {
 	const url = e[1].replace('%s', term)
 	return {
 		id: e[0],
 		title: `Open Search on ${e[2]}`,
 		description: url,
-		icon: findIcon('www'),
+		icon,
 		event: {
 			type: 'open-url',
 			data: url,
@@ -29,29 +27,32 @@ const openSearch = (e:engine, term: string) => {
 	}
 }
 
-const hintSearch = (e:engine) => {
+const hintSearch = (e:engine, icon: string): SearchResult => {
 	return {
 		id: e[0],
 		title: `${e[0]}: search on ${e[2]}`,
-		icon: findIcon('www'),
+		icon,
 		completion: `${e[0]} `,
 	}
 }
 
 const searchEngine = {
 	triggers: Object.freeze(ENGINES.map(e => e[0])),
+	context: undefined as PluginContext,
 
 	async search(term: string, trigger?: string): Promise<SearchResult[]> {
 		const result: SearchResult[] = []
+
+		const icon = `plugin://${this.context.metadata.uid}/search.png`
 		if (term.length > 0) {
 			ENGINES.forEach(e => {
 				// populate the result array with the matched search engine trigger
 				// in case of default search engine, also add hint for other available triggers
 				if (e[0] === trigger) {
-					result.push(openSearch(e, term))
+					result.push(openSearch(e, term, icon))
 				}
 				if (trigger === VOID_TRIGGER && e[0].includes(term)) {
-					result.push(hintSearch(e))
+					result.push(hintSearch(e, icon))
 				}
 			})
 		} else {
@@ -59,9 +60,9 @@ const searchEngine = {
 			// and add default search engine w/ search term = (incomplete) trigger
 			ENGINES.forEach(e => {
 				if (e[0].includes(trigger)) {
-					result.push(hintSearch(e))
+					result.push(hintSearch(e, icon))
 				} else if (e[0] === VOID_TRIGGER) {
-					result.push(openSearch(e, trigger))
+					result.push(openSearch(e, trigger, icon))
 				}
 			})
 		}
@@ -70,8 +71,9 @@ const searchEngine = {
 }
 
 export default (context: PluginContext) => {
+	searchEngine.context = context
 	context.registerSearchEngine(searchEngine, ...searchEngine.triggers)
 	context.registerEventHandler('open-url', (data: any) => {
-		exec(`xdg-open '${data}'`)
+		spawn(`xdg-open '${data}'`)
 	})
 }
