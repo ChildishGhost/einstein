e2148c47806a72dd40c08e10c5333b47e2279559
xatier <xatierlike@gmail.com>
2022-05-29 01:24:43 -0500

    feat(plugin/search): add script to dump chromium search engines

---
+++ plugins/search/dump.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+# Usage: ./dump.sh
+# paste the output into plugins/saerch/index.ts
+
+# This script dumps user defined search engines in Chromium.
+# It replaces {inputEncoding}, which appears in some search engine definitions, with
+# UTF-8, {google:baseURL} with the Google URL, and omits other such tokens.
+
+# Location of Chromium's 'Web Data' SQLite3 file
+#CHROMIUM_WEB_DATA="$HOME/.config/chromium/Default/Web Data"
+CHROMIUM_WEB_DATA="$HOME/.config/microsoft-edge-dev/Default/Web Data"
+
+# Location to create temporary copy of 'Web Data', since the database is locked while
+# Chromium is running
+COPY=$(mktemp)
+
+cp "$CHROMIUM_WEB_DATA" "$COPY"
+
+sqlite3 "$COPY" <<COMMANDS |
+.echo off
+select keyword, url, short_name from keywords;
+.quit
+COMMANDS
+    sed -e \ '
+s#{searchTerms}#%s#g
+s#{google:baseURL}#https://google.com/#g
+s#{inputEncoding}#UTF-8#g
+s#&?[^{}?&]\+={[^}]\+}##g
+s#{[^}]\+}##g
+' |
+    awk -v FS='|' -v q="'" '{ print "[ "q$1q", "q$2q", "q$3q" ],"}'
+rm "$COPY"
